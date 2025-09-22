e683ac6301a5faeab54bdf5bc7aeedfe19f3ec7f
slhkrkya <slhkrkyh@gmail.com>
2025-09-22 13:36:31 +0300

    feat: frontend klasör yapısı ve .gittignore ekle

---
+++ frontend/src/app/modules/meetings/meeting-room/meeting-room.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-room',
+  imports: [],
+  templateUrl: './meeting-room.html',
+  styleUrl: './meeting-room.scss'
+})
+export class MeetingRoom {
+
+}
---
+++ frontend/src/app/modules/meetings/meetings-module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MeetingsRoutingModule } from './meetings-routing-module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MeetingsRoutingModule
+  ]
+})
+export class MeetingsModule { }
